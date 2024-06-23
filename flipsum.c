17b42c2860956ace83b1eb714af881d927176728
Muskra <76000573+Muskra@users.noreply.github.com>
2024-06-23 17:12:30 +0200

    Update
    Now the program is doing nothing for some reason

--- flipsum.c
+++ flipsum.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-#define BUFF_SIZE 1024
+#define GRID_SIZE 32
+#define BUFF_SIZE (GRID_SIZE * GRID_SIZE)
 
 struct Coordinates {
     uint8_t X;
@@ -11,36 +13,47 @@ struct Coordinates {
 
 // flipping the bytes should be with XOR operation (a ^ b) ^ b = a
 // to make the swap from buffer the equation is array[length * row + col] = value
-void flipper(struct Coordinates targets[7], unsigned char *buffer, size_t size) {
-    int coo;
-    int index;
-    for (coo = 0; coo < 7; coo++) {
-        for (index = 0; index < size; index++) {
-            buffer[index] ^= buffer[size * targets[coo].X + targets[coo].Y];
-            buffer[size * targets[coo].X + targets[coo].Y] ^= buffer[index];
-            buffer[index] ^= buffer[size * targets[coo].X + targets[coo].Y];
+void flipper(struct Coordinates targets[7], unsigned char *buffer, size_t size_grid) {
+    int rep, coo, index = 0;
+    size_t size_targets = sizeof(&targets);
+    
+    for (; rep < 128; rep++) {
+        for (; coo < size_targets; coo++) {
+            for (; index < size_buffer; index++) {
+            
+                int targ = size_grid * targets[coo].X + targets[coo].Y;
+                assert(targ > size_grid);
+                buffer[index] ^= buffer[targ];
+                buffer[targ] ^= buffer[index];
+                buffer[index] ^= buffer[targ];
+            }
         }
     }
 }
 
 void encode(FILE *in, FILE *out) {
-    unsigned char buffer[BUFF_SIZE];
-    size_t size;
+    unsigned char buffer[BUFF_SIZE] = {0};
+    size_t size = 0;
 
-    struct Coordinates targets[7] = {
-        {.X = 251, .Y = 7},
-        {.X = 29, .Y = 61},
-        {.X = 97, .Y= 3},
-        {.X = 127, .Y = 101},
-        {.X = 181, .Y = 13},
-        {.X = 47, .Y = 67},
-        {.X = 229, .Y = 73},
+    struct Coordinates targets[9] = {
+        {.X = 12, .Y = 7},
+        {.X = 29, .Y = 6},
+        {.X = 17, .Y= 3},
+        {.X = 27, .Y = 10},
+        {.X = 28, .Y = 23},
+        {.X = 30, .Y = 29},
+        {.X = 2, .Y = 13},
+        {.X = 31, .Y = 26},
+        {.X = 11, .Y = 0},
     };
 
     do {
         size = fread(buffer, 1, BUFF_SIZE, in);
 
-        flipper(targets, buffer, size);
+        // padd the buffer with zeros
+        memset(buffer + size, 0, BUFF_SIZE - size);
+
+        flipper(targets, buffer, GRID_SIZE);
 
         fwrite(buffer, 1, size, out);
     
@@ -49,8 +62,8 @@ void encode(FILE *in, FILE *out) {
 
 int main(int argc, char **argv) {
 
-    char path[128];
-    FILE *in, *out;
+    char path[128] = {0};
+    FILE *in, *out = NULL;
 
     if (argc != 2) {
         printf("Usage : %s [FILE]\n", argv[0]);
